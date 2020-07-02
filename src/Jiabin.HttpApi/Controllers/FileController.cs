30fd80bcaaf69be6da750f4469e03b87ee049112
niuai <1196454838@qq.com>
2020-07-02 20:06:39 +0800

    feat: img的src以及background-image的url都可以重定向

---
+++ src/Jiabin.HttpApi/Controllers/FileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jiabin.Controllers
+{
+    public class FileController : JiabinController
+    {
+        public IActionResult Get()
+        {
+            return Redirect("http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg");
+        }
+    }
+}
